e56efa81487b3cd7d43c843ca4223f465df505a0
tibyishaka <t.ibyishaka@alustudent.com>
2024-10-24 00:27:12 -0700

    sentence

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if  sentence:
+    if sentence:
         s_length = len(sentence)
         f_letter = sentence[0]
     else:
@@ -8,4 +8,3 @@ def multiple_returns(sentence):
         f_letter = None
 
     return tuple((s_length, f_letter))
-
