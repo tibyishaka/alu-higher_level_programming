3d999ac585e5f7c7dcacd4b2e4002fd615ae06a5
tibyishaka <t.ibyishaka@alustudent.com>
2024-10-23 23:28:10 -0700

    printing matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        if not row:
+            print()
+        else:
+            for n in i:
+                if n == i[-1]:
+                    print('{:d}'.format(n))
+                else:
+                    print('{:d}'.format(n), end=" ")
