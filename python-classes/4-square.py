0b57a5589746e7302b69aa51f9b0abe7a6afab19
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-03 23:20:12 -0800

    getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-'''getter and setter '''
+"""Create a getter and a setter using the square class  """
 
 
 class Square:
-    '''init function '''
+    '''
+    Create a square
+        Has a private Instance att: size
+    '''
+
     def __init__(self, size=0):
+        ''' init size '''
         self.__size = size
 
     @property
     def size(self):
+        "returns the size att"
         return self.__size
 
     @size.setter
-    def size(self, value):
+    def size(self, size):
+        '''asign the size to the size att'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size >= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        return self.__size
+        self.__size = size
 
     def area(self):
-        s_area = self.__size ** 2
-        return s_area
+        square_area = self.__size ** 2
+        return square_area
