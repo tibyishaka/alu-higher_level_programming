ca7d098d9ece422eb0e4e5af82e2d7748a17584d
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-03 22:08:20 -0800

    privetae size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class Square:
+    '''creating class with private size attribute'''
+    def __init__(self, size):
+        '''initialising size'''
+        self.__size = size
