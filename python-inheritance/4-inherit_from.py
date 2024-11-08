8c62dc75c476c00cb009512debd1024b4a0ea340
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-07 23:09:13 -0800

    two at once

---
+++ python-inheritance/4-inherit_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''is it the same or inherited '''
+
+
+def inherits_from(obj, class):
+    '''inhertits from '''
+
+
+    return isinstance(obj, a_class) and type(obj) != a_class
+
