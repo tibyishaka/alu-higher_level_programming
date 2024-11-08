443cf1ab71a66fe204f3e018805edf74575297e2
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-07 22:29:09 -0800

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''print sorted list'''
+
+
+class Mylist(list):
+    '''function to print '''
+    def print_sorted(self):
+        sorted_list = self[:]
+        sorted_list.sort()
+        print( "{}".format(sorted_list))
+
