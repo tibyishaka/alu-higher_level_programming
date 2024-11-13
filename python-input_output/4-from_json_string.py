bad5144d9c8355ed344a9f2350c96f967ceb9851
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-13 00:01:50 -0800

    from string to object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-''' function that returns an object (Python data structure) represented by a JSON string'''
+''' returns object (Python data structure) represented by a JSON string:'''
 
 
 import json
 
 
 def from_json_string(my_obj):
-    '''function that returns an object'''
-    return json.roads(my_obj)
+    '''The real function that returns a json obj'''
+    return json.loads(my_obj)
