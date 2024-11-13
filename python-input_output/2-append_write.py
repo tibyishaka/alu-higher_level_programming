4216fe60a5f0507837244d3948f8a75f075aa69b
tibyishaka <t.ibyishaka@alustudent.com>
2024-11-12 23:17:01 -0800

    append to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename=" ", text=""):
     '''function to append string to file'''
-    with open(filename,'a+') as f :
+    with open(filename, 'a+') as f:
         return f.write(text)
